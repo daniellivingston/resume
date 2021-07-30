c421431f51ec59195af04e43cd9f309d9f253f39
Daniel Livingston <daniel.livingston@live.com>
2021-07-30 15:33:37 -0600

    Removed old docker folder, updated website

--- docker/README.md
+++
@@ -1,5 +0,0 @@
-# Dockerfile Compilation
-
-Work in progress. Uses Docker to build and compile LaTeX documents.
-
-Run `./compile.sh` to build and execute the container.
--- docker/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-JOBNAME="livingston_daniel_resume.pdf"
-
-docker build -t pdflatex -f Dockerfile ./
-cd ../
-mkdir build || echo ""
-docker run -it -v $(pwd):/var/local pdflatex bash -c "pdflatex -jobname=${JOBNAME} main.tex"
